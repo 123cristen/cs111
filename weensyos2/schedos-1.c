60ae36f967644f4fe2ac409c8fc3e4c7083d87fe
Cristen Anderson <1cristen1@gmail.com>
2016-03-04 12:11:36 -0800

    exercise 2

--- weensyos2/schedos-1.c
+++ weensyos2/schedos-1.c
@@ -38,8 +38,8 @@ start(void)
 		*cursorpos++ = PRINTCHAR;
 		sys_yield();
 	}
-
+	sys_exit();
 	// Yield forever.
-	while (1)
-		sys_yield();
+	// while (1)
+	// 	sys_yield();
 }
--- weensyos2/schedos-kern.c
+++ weensyos2/schedos-kern.c
@@ -127,7 +127,7 @@ start(void)
 	//   41 = p_priority algorithm (exercise 4.a)
 	//   42 = p_share algorithm (exercise 4.b)
 	//    7 = any algorithm that you may implement for exercise 7
-	scheduling_algorithm = 0;
+	scheduling_algorithm = 2;
 
 	// Switch to the first process.
 	run(&proc_array[1]);
@@ -218,6 +218,7 @@ void
 schedule(void)
 {
 	pid_t pid = current->p_pid;
+	int i;
 
 	if (scheduling_algorithm == 0)
 		while (1) {
@@ -229,6 +230,14 @@ schedule(void)
 			if (proc_array[pid].p_state == P_RUNNABLE)
 				run(&proc_array[pid]);
 		}
+	else if (scheduling_algorithm == __EXERCISE_2__) {
+		while (pid < NPROCS) {
+			if(proc_array[pid].p_state == P_RUNNABLE)
+				run(&proc_array[pid]);
+			else
+				pid = (pid + 1) % NPROCS;
+		}
+	}
 
 	// If we get here, we are running an unknown scheduling algorithm.
 	cursorpos = console_printf(cursorpos, 0x100, "\nUnknown scheduling algorithm %d\n", scheduling_algorithm);
