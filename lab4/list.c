118fd940ae0afb8c3c9a4095bfab3499fba3f6bb
Cristen Anderson <1cristen1@gmail.com>
2016-03-06 15:01:34 -0800

    Add SortedList.h, list.c. Modify makefile

---
+++ lab4/list.c
@@ -0,0 +1,11 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <getopt.h>
+#include <pthread.h>
+#include <string.h>
+
+#include "SortedList.h"
+
