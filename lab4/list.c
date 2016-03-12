6a3dfb79b7a9ec13e1284a9a8cce046502719813
Cristen Anderson <1cristen1@gmail.com>
2016-03-11 22:27:23 -0800

    add printf statement

--- lab4/list.c
+++ lab4/list.c
@@ -377,6 +377,7 @@ int main(int argc, char **argv) {
   endTime = (long long)(end.tv_sec*pow(10, 9) + end.tv_nsec);
   startTime = (long long) (start.tv_sec*pow(10, 9) + start.tv_nsec);
   threadTime = (long long) ((ethreads.tv_sec*pow(10, 9) + end.tv_nsec) - (sthreads.tv_sec*pow(10, 9) + start.tv_nsec))
+  printf("total: %lld, thread: %lld\n",endTime-startTime, threadTime);
   totalTime = endTime-startTime-threadTime;
   operations = num_elements*2 + num_threads;
   int finalLength = SortedList_length(lists);
